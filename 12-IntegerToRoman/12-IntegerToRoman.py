f19d303f2631044f77482545358838aeb45feb3f
Dinesh <dinesh123pandi@gmail.com>
2026-07-14 09:57:38 +0530

    Create 12-IntegerToRoman

---
+++ 12-IntegerToRoman/12-IntegerToRoman.py
@@ -0,0 +1,11 @@
+# Last updated: 7/14/2026, 9:57:38 AM
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        roman=['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+        r=""
+        for i in range(len(values)):
+            while num>=values[i]:
+                r+=roman[i]
+                num-=values[i]
+        return r
