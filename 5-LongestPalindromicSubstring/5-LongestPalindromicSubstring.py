7c669afbae88331d2da6eda92888d1705693323c
Dinesh <dinesh123pandi@gmail.com>
2026-07-14 09:57:44 +0530

    Create 5-LongestPalindromicSubstring

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -0,0 +1,10 @@
+# Last updated: 7/14/2026, 9:57:44 AM
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest=""
+        for i in range(len(s)):
+            for j in range(i,len(s)):
+                substring=s[i:j+1]
+                if substring==substring[::-1] and len(substring)>len(longest):
+                    longest=substring
+        return longest
