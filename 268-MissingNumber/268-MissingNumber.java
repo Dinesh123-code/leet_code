cd7155b770c983c25be6701ee378f86eee90de11
Dinesh <dinesh123pandi@gmail.com>
2026-07-14 09:56:35 +0530

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 7/14/2026, 9:56:35 AM
+class Solution {
+    public int missingNumber(int[] nums) {
+        for(int i=0;i<=nums.length;i++){  
+            boolean flag=false;
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]==i){
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag==false){
+                return i;  
+            }
+        }
+        return -1;
+    }
+}
