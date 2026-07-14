656f517f02cbc2f9555575391e46cc225eb0b682
Dinesh <dinesh123pandi@gmail.com>
2026-07-14 09:55:38 +0530

    Create 2432-NumberOfZeroFilledSubarrays

---
+++ 2432-NumberOfZeroFilledSubarrays/2432-NumberOfZeroFilledSubarrays.py
@@ -0,0 +1,12 @@
+# Last updated: 7/14/2026, 9:55:39 AM
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        c=0
+        ans=0
+        for i in nums:
+            if i==0:
+                c+=1
+                ans+=c
+            else:
+                c=0
+        return ans
