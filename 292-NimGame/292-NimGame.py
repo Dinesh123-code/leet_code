57966c9a50954c5a2539b10a1a5ce5e5ca032e40
Dinesh <dinesh123pandi@gmail.com>
2026-07-14 09:56:24 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.py
@@ -0,0 +1,7 @@
+# Last updated: 7/14/2026, 9:56:24 AM
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        if n%4==0:
+            return False
+        else:
+            return True
