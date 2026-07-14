3235a15fa17dd50e0ef609fa596f0632ff78ad5f
Dinesh <dinesh123pandi@gmail.com>
2026-07-14 09:57:23 +0530

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.py
@@ -0,0 +1,15 @@
+# Last updated: 7/14/2026, 9:57:23 AM
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l=0
+        r=0
+        res=0
+        while r<len(nums)-1:
+            m=0
+            for i in range(l,r+1):
+                if nums[i]+i>m:
+                    m=i+nums[i]
+            l=r+1
+            r=m
+            res+=1
+        return res
